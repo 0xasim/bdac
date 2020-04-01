47f18b7e2043acecfbea3c09948bcb0397359943
dude <asimalilatif@gmail.com>
2020-04-01 20:54:03 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Bulk Domain Availability Checking Tool
+
+## Installation
+
+### Requirements
+
+-python3
+-pip3
+
+### Setup
+> Install requirements first
+```
+$ pip3 install -r requirements.txt
+```
+
+### Usage
+```
+$ ./bdac.py
+usage: bdac.py [-h] [--file FILE] [--domains DOMAINS [DOMAINS ...]] [--socket]
+               [--whois]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --file FILE, -f FILE  Path to file containing domains
+  --domains DOMAINS [DOMAINS ...], -d DOMAINS [DOMAINS ...]
+                        List of domains
+  --socket, -s          Use socket method for checking availability. Note: It
+                        return available if it fails to resolve domain names
+                        to their namesevers and unavailable if domain resolves
+                        (It won't show grabbed domains that are available)
+  --whois, -w           Use whois method for checking availability. Note:
+                        There are limited tries an IP can have (No official
+                        number but about some estimate about 1000/day)
+```
+### Examples
+```
+Socket method and domains list
+$ ./bdac.py -s -d google.com quora.com
+
+Whois method and domains file
+$ ./bdac.py -w -f domains.txt
+
+Both methods and domains file
+$ ./bdac.py -s -w -f domains.txt
+```
+
+### Note
+Tested on Ubuntu 18.04
+whois has about 1000 requests limit per day
+
+### TODO
+handle file path from different directory accross any os
+
---
+++ bdac.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import socket
+import sys
+import argparse
+import subprocess as sp
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', '-f', help="Path to file containing domains")
+parser.add_argument('--domains', '-d', help="List of domains", nargs='+')
+parser.add_argument('--socket', '-s', help="Use socket method for checking availability. Note: It return available if it fails to resolve domain names to their namesevers and unavailable if domain resolves (It won't show grabbed domains that are available)", action="store_true")
+parser.add_argument('--whois', '-w', help="Use whois method for checking availability. Note: There are limited tries an IP can have (No official number but about some estimate about 1000/day)", action="store_true")
+args = parser.parse_args()
+argsDict= vars(args)
+
+#help printing
+if len(sys.argv)==1:
+	parser.print_help()
+	sys.exit(0)
+
+def parseFile(filepath):
+	domains = []
+	try:
+		file = open(filepath, "r")
+		readfile = file.readlines()
+	except FileNotFoundError:
+		print("File not found")
+		#raise FileNotFoundError
+		sys.exit(0)
+	
+	for each in readfile:
+		domains.append(each.rstrip('\n'))
+	return domains
+
+def getDomains(argsDict):
+	#if file is proided as argument
+	if argsDict["file"]:
+		domains = parseFile(argsDict["file"])
+
+	#if domains are proided as argument
+	elif argsDict["domains"]:
+		domains = argsDict["domains"]
+
+	return domains
+
+def usingSocket(domains):
+	print("Using Socket:\n")
+	for each in domains:
+		try:
+			socket.getaddrinfo(each,None)
+			print('\tNot available: {domain}'.format(domain=each))
+
+		#not working
+		except socket.timeout:
+			print("connection timeout")
+		except socket.gaierror:
+			print('\tAvailable: {domain}:'.format(domain=each))
+		except:
+			print("Some other error Occured in socket")
+
+def usingWhois(domains):
+	print("Using Socket:\n")
+	for each in domains:
+		try:
+			sp.check_output('whois %s'%each, shell=True)
+			print('\tNot available: {domain}'.format(domain=each))
+		except sp.CalledProcessError:
+			print('\tAvailable: {domain}'.format(domain=each))
+		except:
+			print("Some error Occured in whois")
+
+def main():
+	domains = getDomains(argsDict)
+	if argsDict["socket"]:
+		usingSocket(domains)
+
+	if argsDict["whois"]:
+		usingWhois(domains)
+
+if __name__ == "__main__":
+	main()
